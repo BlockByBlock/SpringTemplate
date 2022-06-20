87a54011a197d77a9f1d5acfdb6ea4aa22f23e45
blockbyblock <ybingcheng@gmail.com>
2022-06-20 13:55:58 +0800

    init commit

---
+++ src/main/java/com/blockbyblock/springtemp/SpringtempApplication.java
@@ -0,0 +1,13 @@
+package com.blockbyblock.springtemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blockbyblock/springtemp/SpringtempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockbyblock.springtemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
