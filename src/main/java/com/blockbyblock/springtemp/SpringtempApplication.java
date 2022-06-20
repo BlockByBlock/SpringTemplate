4667de06922537a7b0021ef5bbb1c41f59ba3b90
blockbyblock <ybingcheng@gmail.com>
2022-06-20 14:55:54 +0800

    Add entity TempModel

--- src/main/java/com/blockbyblock/springtemp/SpringtempApplication.java
+++ src/main/java/com/blockbyblock/springtemp/SpringtempApplication.java
@@ -1,13 +1,34 @@
 package com.blockbyblock.springtemp;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blockbyblock.springtemp.TempModel.TempModel;
 
 @SpringBootApplication
-public class SpringtempApplication {
+@RestController
+public class SpringTempApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringtempApplication.class, args);
+		SpringApplication.run(SpringTempApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+
+	@GetMapping("/temp")
+	public List<TempModel> temp() {
+		return List.of(
+				new TempModel("temp1", LocalDate.now()),
+				new TempModel("temp2", LocalDate.now()),
+				new TempModel("temp3", LocalDate.now())
+		);
+	}
 }
---
+++ src/main/java/com/blockbyblock/springtemp/TempModel/TempModel.java
@@ -0,0 +1,25 @@
+package com.blockbyblock.springtemp.TempModel;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TempModel {
+    private Long id;
+    private String name;
+    private LocalDate createdTime;
+
+    public TempModel(String name, LocalDate createdTime) {
+        this.name = name;
+        this.createdTime = createdTime;
+    }
+}
