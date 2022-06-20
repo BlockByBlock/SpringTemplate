4404b5118d789be19f2f5eb7b604520ce98e1d14
blockbyblock <ybingcheng@gmail.com>
2022-06-20 22:09:19 +0800

    test repository

---
+++ src/test/java/com/blockbyblock/springtemp/TempModel/TempModelRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.blockbyblock.springtemp.TempModel;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+public class TempModelRepositoryTest {
+
+  @Autowired
+  private TempModelRepository tempModelRepository;
+
+  @AfterEach
+  void tearDown() {
+    tempModelRepository.deleteAll();
+  }
+
+  @Test
+  void itShouldSaveTempModel() {
+    String name = "test1";
+    TempModel tempModel = new TempModel(
+      name, LocalDate.now()
+    );
+
+    tempModelRepository.save(tempModel);
+    boolean expected = tempModelRepository.findByName(name).isPresent();
+
+    assertThat(expected).isTrue();
+  }
+
+  @Test
+  void itShouldFailIfTempModelNameDoesNotExists() {
+    String name = "test1";
+
+    boolean expected = tempModelRepository.findByName(name).isPresent();
+
+    assertThat(expected).isFalse();
+  }
+}
